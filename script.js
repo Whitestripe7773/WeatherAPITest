2f8f8938da08e421981ce58b55da886e2ad290c2
Daniel <whitestripe7773@gmail.com>
2020-05-17 21:08:00 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,88 @@
+/**
+ * Function to handle the logic
+ */
+
+function getData(){
+    if (getCity().length == 0 && getZipCode().length == 0){
+        alert("Please fill atleast one textfield!");
+    }
+    else if (getCity().length > 0){
+        showDataWithCityName();
+    }
+    else if (getCity().length > 0 && getZipCode().length > 0){
+        showDataWithZIP();
+    }
+    else{
+        showDataWithZIP();
+    }
+}
+
+/**
+ * Function which shows the weather data by city name
+ */
+function showDataWithCityName(){
+$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=" + getCity() + "&units=metric&appid=" + getAPIKey(), showdata = function(data){
+
+    // Getting the icon from the JSON object as String
+    var icon = "http:/openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+    // Getting the Temperatue from the JSON object
+    var temperature = Math.floor(data.main.temp);
+    // Getting the weather status from the JSON object
+    var weatherStatus = data.weather[0].main;
+
+    // In die class "weather-icon" in der index HTML packen wir das att(src, icon) rein
+    $(".weather-icon").attr("src", icon);
+    $(".weather-status").text(weatherStatus);
+    $(".temperature").text(temperature +"°C");
+    $(".city-name").text(getCity());
+});
+}
+
+/**
+ * Function which shows the weather data by ZIP code
+ */
+function showDataWithZIP(){
+    $.getJSON("http://api.openweathermap.org/data/2.5/weather?zip=" + getZipCode() + "," + getCountry() + "&units=metric&appid=" + getAPIKey(), showdata = function(data){
+    
+        // Getting the icon from the JSON object as String
+        var icon = "http:/openweathermap.org/img/w/" + data.weather[0].icon + ".png";
+        // Getting the Temperatue from the JSON object
+        var temperature = Math.floor(data.main.temp);
+        // Getting the weather status from the JSON object
+        var weatherStatus = data.weather[0].main;
+    
+        // In die class "weather-icon" in der index HTML packen wir das att(src, icon) rein
+        $(".weather-icon").attr("src", icon);
+        $(".weather-status").text(weatherStatus);
+        $(".temperature").text(temperature + "°C");
+        $(".city-name").text(getCity());
+    });
+    }
+
+/**
+ * Getter
+ */
+
+ // Get text from cityname input
+function getCity(){
+    var city = document.getElementById("cityName").value;
+    return city;
+}
+
+// Get text from zipcode input
+function getZipCode(){
+    var zip = document.getElementById("zipCode").value;
+    return zip;
+}
+
+// Get selected country from dropdown
+function getCountry(){
+    var country = document.getElementById("country").value;
+    return country;
+}
+
+// Get APIKey (you can insert your own key in here)
+function getAPIKey(){
+    var API_KEY = "e907f577e7c0ef5bf8a4f7ea0562fb66";
+    return API_KEY;
+}
